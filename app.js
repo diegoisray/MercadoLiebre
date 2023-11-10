4f6d934eb3b6c71735a358ec4e36d2b09e367cac
diegoisray <diegoisray2@gmail.com>
2023-11-09 21:11:16 -0300

    agregue archivos para los formularios de registro y logueado, cambie algo del html

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(4000, () => {
     console.log("servidor corriendo en puerto 4000")
@@ -11,4 +14,21 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.post('/register', (req,res) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.post('/login', (req,res) => {
+    console.log(req.body);
+    res.redirect('/')
+})
 app.use(express.static('public'))
